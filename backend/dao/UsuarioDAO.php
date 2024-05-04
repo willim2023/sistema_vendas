594740e9bf27a2479f05f2ff4512e2c081ecb212
willim2023 <paul.willim.2018@gmail.com>
2024-05-03 22:09:41 -0300

    Usuario DAO CRUD completo

--- backend/dao/UsuarioDAO.php
+++ backend/dao/UsuarioDAO.php
@@ -1,7 +1,8 @@
 <?php
 
-require_once '../config/Database.php';
-require_once '../entity/Usuario.php';
+require_once 'config/Database.php';
+require_once 'entity/Usuario.php';
+require_once 'BaseDAO.php';
 
 class UsuarioDAO implements BaseDAO {
     private $db;
@@ -72,16 +73,76 @@ class UsuarioDAO implements BaseDAO {
         }
     }
 
-    public function create($entity) {
+    public function create($usuario) {
+        try {
+            // Preparar a consulta SQL
+            $sql = "INSERT INTO Usuario( NomeUsuario , Senha , Email , GrupoUsuarioID , Ativo , DataCriacao , DataAtualizacao , UsuarioAtualizacao )
+                    VALUES(:nomeUsuario, :senha, :email, :grupoUsuarioID, :ativo, current_timestamp(),current_timestamp(),null)";
 
+            // Preparar a instrução
+            $stmt = $this->db->prepare($sql);
+
+            // Vincular parâmetros
+            $stmt->bindParam(':nomeUsuario', $usuario->getNomeUsuario());
+            $stmt->bindParam(':senha', $usuario->getSenha());
+            $stmt->bindParam(':email', $usuario->getEmail());
+            $stmt->bindParam(':grupoUsuarioID', $usuario->getGrupoUsuarioId());
+            $stmt->bindParam(':ativo', $usuario->getAtivo());
+            
+            // Executar a instrução
+            $stmt->execute();
+
+            // Retornar verdadeiro se a inserção for bem sucedida
+            return true;
+        } catch (PDOException $e) {
+            // TO-DO: implementar log
+            return false;
+        }
     }
 
-    public function update($entity) {
+    public function update($usuario) {
+        try {
+            // Verifico se o usuário existe no banco de dados
+            $existingUser = $this->getById($usuario->getId());
 
+            if(!$existingUser) {
+                return false;// Retorna falso se o usuário não existir
+            }
+
+            $sql = "UPDATE Usuario SET NomeUsuario = :nomeUsuario, Senha = :senha, Email = :email,
+            GrupoUsuarioID = :grupoUsuarioID, Ativo = :ativo, DataAtualizacao = current_timestamp()
+            WHERE Id = :id";
+
+            $stmt = $this->db->prepare($sql);
+
+            $stmt->bindParam(':id', $usuario->getId());
+            $stmt->bindParam(':nomeUsuario', $usuario->getNomeUsuario());
+            $stmt->bindParam(':senha', $usuario->getSenha());
+            $stmt->bindParam(':email', $usuario->getEmail());
+            $stmt->bindParam(':grupoUsuarioID', $usuario->getGrupoUsuarioId());
+            $stmt->bindParam(':ativo', $usuario->getAtivo());
+
+            $stmt->execute();
+
+            return true;
+
+        } catch (PDOException $e) {
+            // TO-DO: implementar log
+            return false;
+        }
     }
 
     public function delete($id) {
+        try {
+            $sql = "DELETE FROM Usuario WHERE Id = :id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
 
+            return true;
+        } catch (PDOException $e) {
+            return false;
+        }
     }
 }
 
--- backend/index.php
+++ backend/index.php
@@ -1,9 +1,17 @@
 <?php
-require_once("template/header.php");
+    require_once("template/header.php");
+    require_once 'dao/UsuarioDAO.php';
+
+    $usuarioDao = new UsuarioDAO();
+    if($usuarioDao->getById(1)) {
+      echo "Usuario Existe";
+  } else {
+      echo "Usuario Não existe";
+  }
 ?>
-   <h1>Olá Sistema Vendas Body</h1>
+    <h1>Olá Sistema Vendas Body</h1>
 </body>
 
 <?php
-  require_once("template/footer.php");
+    require_once("template/footer.php");
 ?>
