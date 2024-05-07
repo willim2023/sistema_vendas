b900f32152c6e1614534753ace0640d750be9ba1
willim2023 <paul.willim.2018@gmail.com>
2024-05-07 20:23:27 -0300

    CRUD completo do GrupoUsuarioDAO

--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,16 @@
 <?php
     require_once("template/header.php");
+    require_once 'dao/GrupoUsuarioDAO.php';
+    require_once 'entity/GrupoUsuario.php';
+
+
+    $grupoUsuarioDao = new GrupoUsuarioDAO();
+
+    //echo $grupoUsuarioDao->getById(1)->getDataCriacao();
+    //print_r($grupoUsuarioDao->getAll());
+    $novoGrupoUsuario = new GrupoUsuario(null, "Generico", "Usuario Padrão da Aplicação", null, null, 1);
+    echo $grupoUsuarioDao->create($novoGrupoUsuario);
+
 ?>
     <h1>Olá Sistema Vendas Body</h1>
 </body>
