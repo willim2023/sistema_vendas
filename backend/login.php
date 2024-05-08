762df2b50d1e38a88b40c930bf51c66bd49c87be
willim2023 <paul.willim.2018@gmail.com>
2024-05-07 21:48:08 -0300

    Adicao de arquivos css para o footer

---
+++ backend/login.php
@@ -0,0 +1,7 @@
+<?php
+    require_once("template/header.php");
+?>
+    <h1>PLACEHOLDER DA PÁGINA DE LOGIN</h1>
+<?php
+    require_once("template/footer.php");
+?>
---
+++ backend/sobre.php
@@ -0,0 +1,7 @@
+<?php
+    require_once("template/header.php");
+?>
+    <h1>Página de Estudos Senac 2024</h1>
+<?php
+    require_once("template/footer.php");
+?>
